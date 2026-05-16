2d201dd57d602358dd5dc7bdc99b9036588f3f45
2003Vandu <patilvandesh301@gmail.com>
2026-05-16 20:52:57 +0530

    updated Endpoint

--- src/main/java/com/Spring/AI/FirstProject/SpringAI/Controller/AiController.java
+++ src/main/java/com/Spring/AI/FirstProject/SpringAI/Controller/AiController.java
@@ -1,18 +1,21 @@
 package com.Spring.AI.FirstProject.SpringAI.Controller;
 
 import com.Spring.AI.FirstProject.SpringAI.Service.GeminiAiService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/ai")
+@RequiredArgsConstructor
 public class AiController {
 
-    @Autowired
-    private GeminiAiService aiService;
+    private final GeminiAiService aiService;
 
     @GetMapping("/chat")
-    public String chat(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    public String chat(
+            @RequestParam(defaultValue = "Hello! What can you help me with?")
+            String message) {
         return aiService.generateAnswer(message);
     }
 }
